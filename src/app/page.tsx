debc6b073bf2273166c61e09a848b1273c8eff7f
LemurSage <zainjee37405@gmail.com>
2025-05-19 04:36:48 +0500

    feat(tailwind): Removed scss, added tailwind, and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 'use client';
-import Canvas from '@/lib/components/Canvas';
-import Forms from '@/lib/components/Forms';
-import { Progress } from '@/lib/components/Progress';
-import StoreProvider from '@/lib/components/StoreProvider';
+import Canvas from '@/components/Canvas';
+import Forms from '@/components/Forms';
+import { Progress } from '@/components/Progress';
+import StoreProvider from '@/components/StoreProvider';
 import { defaultCanvasVars } from '@/lib/types/CanvasVars';
-import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <StoreProvider>
-      <main className={styles.main} style={{ width: `${defaultCanvasVars.canvasWd}px` }}>
+      <main className="w-fit mx-auto py-20" style={{ width: `${defaultCanvasVars.canvasWd}px` }}>
         <Canvas />
         <Progress />
         <Forms />
